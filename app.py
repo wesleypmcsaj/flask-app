40a943b6d77b6de853a479b59ac9e96044e7439c
wesleypmcsaj <wesleypmcsaj@gmail.com>
2024-09-26 15:39:57 -0300

    app.py

--- app.py
+++ app.py
@@ -16,9 +16,9 @@ scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/au
 # A chave de API deve ser lida a partir de uma variável de ambiente
 google_api_key = os.getenv('GOOGLE_API_KEY')  # Obtendo a chave da variável de ambiente
 
-# Se a chave de API for necessária para a autenticação,
+# Se a chave de API for necessária para a autenticação, 
 # você deve garantir que ela seja usada corretamente aqui.
-# Caso você ainda precise do arquivo JSON para credenciais,
+# Caso você ainda precise do arquivo JSON para credenciais, 
 # ele deverá estar presente no seu repositório ou no ambiente onde o app é executado.
 creds = ServiceAccountCredentials.from_json_keyfile_name('copia-arquivos-para-o-gdrive-9d3fd7c778f6.json', scope)  # Substitua pelo caminho do seu arquivo JSON
 client = gspread.authorize(creds)
@@ -27,13 +27,13 @@ client = gspread.authorize(creds)
 def index():
     if request.method == 'GET':
         return render_template('index.html')
-
+    
     if request.method == 'POST':
         try:
             data = request.get_json()
             qr_data = data.get('data')
             ano_atual = datetime.now().year
-
+            
             spreadsheet = client.open_by_key('16_zlC5bRdyGTqFcVFvRIBCYzP-fjoPN9i64tD5DGe5c')  # ID da planilha
             worksheet = spreadsheet.worksheet('Atual')  # Nome da aba
 
@@ -41,7 +41,7 @@ def index():
             if qr_data.startswith("OF"):
                 num_oficio = qr_data.split(" ")[1]
                 termo_busca = f"{num_oficio}/SAJ/{ano_atual}"
-
+                
                 # Buscar o termo na coluna A
                 cell = worksheet.find(termo_busca, in_column=1)  # Coluna A é a 1ª
                 if cell:
@@ -51,11 +51,11 @@ def index():
                     resultado = {'success': True, 'message': "Atualização feita com sucesso."}
                 else:
                     resultado = {'success': False, 'message': "Documento não encontrado."}
-
+            
             elif qr_data.startswith("CI"):
                 num_ci = qr_data.split(" ")[1]
                 termo_busca = f"CI {num_ci}-SAJ-{ano_atual}"
-
+                
                 # Buscar o termo na coluna O
                 cell = worksheet.find(termo_busca, in_column=15)  # Coluna O é a 15ª
                 if cell:
@@ -67,7 +67,7 @@ def index():
                     resultado = {'success': False, 'message': "Documento não encontrado."}
             else:
                 resultado = {'success': False, 'message': "Tipo de documento não reconhecido."}
-
+            
             return jsonify(resultado)
 
         except Exception as e:
