68846b4e016e8be9a28365316e1bb7812be0e089
wesleypmcsaj <wesleypmcsaj@gmail.com>
2024-09-26 11:24:39 -0300

    app.py

--- app.py
+++ app.py
@@ -2,19 +2,12 @@ from flask import Flask, render_template, request, jsonify
 from datetime import datetime
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-from flask_cors import CORS  # Importando a biblioteca Flask-CORS
+from flask_cors import CORS
 
 app = Flask(__name__)
 
-# Configure o CORS para permitir origens específicas
-# Adicionando todos os links de acesso permitidos no CORS
-CORS(app, resources={r"/*": {"origins": [
-    "https://flask-app-blush-one.vercel.app", 
-    "https://flask-app-psi-red.vercel.app",
-    "https://flask-app-tau-wheat.vercel.app",
-    "https://flask-app-git-main-wesleypmcsajs-projects.vercel.app",
-    "https://flask-cfzfcqkw3-wesleypmcsajs-projects.vercel.app"
-]}})
+# Permitir CORS de qualquer origem
+CORS(app)
 
 # Configurações de autenticação para acessar a planilha Google
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
@@ -24,30 +17,24 @@ client = gspread.authorize(creds)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
-        # Renderiza o arquivo HTML na rota '/'
         return render_template('index.html')
     
     if request.method == 'POST':
-        # Lida com a requisição POST (o QR Code escaneado)
         data = request.get_json()
         qr_data = data.get('data')
         ano_atual = datetime.now().year
         
-        # Acessa a planilha pelo ID
         spreadsheet = client.open_by_key('16_zlC5bRdyGTqFcVFvRIBCYzP-fjoPN9i64tD5DGe5c')  # ID da planilha
         worksheet = spreadsheet.worksheet('Atual')  # Nome da aba
 
-        # Lógica similar à que você tinha no Google App Script
         resultado = {}
         if qr_data.startswith("OF"):
             num_oficio = qr_data.split(" ")[1]
             termo_busca = f"{num_oficio}/SAJ/{ano_atual}"
-            # Aqui você deve adicionar a lógica para atualizar a planilha
             resultado = {'success': True, 'message': f"Atualização feita com sucesso para {termo_busca}."}
         elif qr_data.startswith("CI"):
             num_ci = qr_data.split(" ")[1]
             termo_busca = f"CI {num_ci}-SAJ-{ano_atual}"
-            # Aqui você deve adicionar a lógica para atualizar a planilha
             resultado = {'success': True, 'message': f"Atualização feita com sucesso para {termo_busca}."}
         else:
             resultado = {'success': False, 'message': "Tipo de documento não reconhecido."}
